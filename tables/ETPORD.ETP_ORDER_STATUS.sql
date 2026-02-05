fb888e0278b80a28ecd92e58d0ae8548738d79dd
Emad.Ragheb <emaderagheb@gmail.com>
2026-02-05 14:03:09 +0200

    Add new order status codes to ETP_ORDER_STATUS table
    - Extended the ETPORD.ETP_ORDER_STATUS table with additional status codes including 'Queued', 'Reject AtLoad', 'Global CXL', and others.
    - Enhanced order management capabilities by providing a comprehensive list of order statuses for better tracking and processing.

--- tables/ETPORD.ETP_ORDER_STATUS.sql
+++ tables/ETPORD.ETP_ORDER_STATUS.sql
@@ -15,3 +15,32 @@ create table ETPORD.ETP_ORDER_STATUS
 | 786 | Complete Fill | Complete Fill |
 | 789 | Suspended | Suspended |
 | 790 | Resumed - OPEN | Resumed - OPEN |
+CODE	DESC
+772	Queued        
+773	Reject AtLoad 
+774	Global CXL    
+775	Changed       
+776	Canceled      
+777	FOK           
+778	Expired       
+779	Filled        
+780	Trade CXL     
+782	Spot CXL      
+783	Accepted      
+784	Rej on Activate
+785	Activated     
+786	Complete Fill 
+787	Family Changed
+788	Price Adjusted
+789	Suspended     
+790	Resumed       
+791	Global Suspend
+792	Global Resume 
+793	Rej on Resume 
+794	CFO From      
+795	CFO To        
+796	Reinstated    
+797	Move From     
+798	Move To       
+800	ErrorCorrection
+804	Corp Act Susp
